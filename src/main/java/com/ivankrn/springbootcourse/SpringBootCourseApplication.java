9fb697730f19c5a0c7124761a23f1af90358a563
ivankrn <kropotiniv@yandex.ru>
2023-03-19 17:52:52 +0500

    Убрал неиспользуемые импорты

--- src/main/java/com/ivankrn/springbootcourse/SpringBootCourseApplication.java
+++ src/main/java/com/ivankrn/springbootcourse/SpringBootCourseApplication.java
@@ -1,14 +1,12 @@
 package com.ivankrn.springbootcourse;
 
-import com.ivankrn.springbootcourse.model.Laptop;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootCourseApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCourseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCourseApplication.class, args);
+    }
 
 }
