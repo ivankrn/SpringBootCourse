99f89e889d1bf7dcc9ab087fdfd418904415ca94
ivankrn <kropotiniv@yandex.ru>
2023-04-18 19:46:04 +0500

    Security homework

--- src/main/java/com/ivankrn/springbootcourse/config/DevDBInitializer.java
+++ src/main/java/com/ivankrn/springbootcourse/config/DevDBInitializer.java
@@ -2,12 +2,15 @@ package com.ivankrn.springbootcourse.config;
 
 import com.ivankrn.springbootcourse.database.BugGroupRepository;
 import com.ivankrn.springbootcourse.database.BugRepository;
+import com.ivankrn.springbootcourse.database.UserRepository;
 import com.ivankrn.springbootcourse.model.Bug;
 import com.ivankrn.springbootcourse.model.BugGroup;
+import com.ivankrn.springbootcourse.model.MyUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Profile("dev")
@@ -15,9 +18,16 @@ import org.springframework.context.annotation.Profile;
 public class DevDBInitializer implements CommandLineRunner {
     private final BugGroupRepository bugGroupRepository;
     private final BugRepository bugRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
+        saveBugs();
+        saveUsers();
+    }
+
+    private void saveBugs() {
         BugGroup frontendGroup = new BugGroup();
         frontendGroup.setName("Frontend");
         bugGroupRepository.save(frontendGroup);
@@ -41,4 +51,16 @@ public class DevDBInitializer implements CommandLineRunner {
         bugRepository.save(endpointBug);
     }
 
+    private void saveUsers() {
+        MyUser user = new MyUser();
+        user.setUsername("user");
+        user.setPassword(passwordEncoder.encode("user_password"));
+        user.setRole("USER");
+        userRepository.save(user);
+        MyUser admin = new MyUser();
+        admin.setUsername("admin");
+        admin.setPassword(passwordEncoder.encode("admin_password"));
+        admin.setRole("ADMIN");
+        userRepository.save(admin);
+    }
 }
