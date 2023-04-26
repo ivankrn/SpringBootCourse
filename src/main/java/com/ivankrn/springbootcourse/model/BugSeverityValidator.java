ee6693c056e08d8bc6c7ae664309d816005e4b2f
ivankrn <kropotiniv@yandex.ru>
2023-04-26 18:45:18 +0500

    Поправил

--- src/main/java/com/ivankrn/springbootcourse/model/BugSeverityValidator.java
+++ src/main/java/com/ivankrn/springbootcourse/model/BugSeverityValidator.java
@@ -5,11 +5,6 @@ import javax.validation.ConstraintValidatorContext;
 
 public class BugSeverityValidator implements ConstraintValidator<SeverityConstraint, Integer> {
     @Override
-    public void initialize(SeverityConstraint constraintAnnotation) {
-        ConstraintValidator.super.initialize(constraintAnnotation);
-    }
-
-    @Override
     public boolean isValid(Integer value, ConstraintValidatorContext context) {
         return value >= 0;
     }
