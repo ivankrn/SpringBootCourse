0b1c301f4b538d08f991906ed9a876a1db81d531
ivankrn <kropotiniv@yandex.ru>
2023-04-27 08:24:37 +0500

    Убрал закоментированный код

--- src/main/java/com/ivankrn/springbootcourse/config/OsChecker.java
+++ src/main/java/com/ivankrn/springbootcourse/config/OsChecker.java
@@ -2,14 +2,11 @@ package com.ivankrn.springbootcourse.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
-//@ConditionalOnProperty(prefix = "info", name = "launchedOn", havingValue = "Windows_NT")
-//@ConditionalOnProperty(prefix = "info", name = "launchedOn")
 @ConditionalOnExpression("!'${info.launchedOn}'.equals('default')")
 @Slf4j
 public class OsChecker {
